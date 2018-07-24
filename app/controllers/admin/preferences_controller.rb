94c95787c44f1629afa868ba0d6e7babcff391fd
ryanwevans <ryanwevans@icloud.com>
2018-07-23 20:57:00 -0700

    add #index, #update and #preference_params to PreferencesController; remove notes from preference.rb

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,7 +1,17 @@
 class Admin::PreferencesController < ApplicationController
 
   def index
-    @preferences = Preference.all
+    @preferences = Preference.first_or_create(artist_sort_order: "ASC", song_sort_order: "ASC", allow_create_artist: false, allow_create_song: false)
+  end
+
+  def update
+    @preferences = Preference.update(params[preference_params])
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit(:artist_sort_order, :song_sort_order, :allow_create_artist, :allow_create_song)
   end
 
 end
--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,4 +1,3 @@
 class Preference < ActiveRecord::Base
-  # will store preferences for the app
-  # store these in a helper folder for access by views?
+  
 end
